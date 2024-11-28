549515fbf34d5b30e9fe0e355dc52dfa214b7ed6
Akihiro Kobayashi <ak-kobayashi@kddi-research.jp>
2024-11-28 19:04:12 +0900

    ・【UIの記述】->【リストのレンダー】のプロジェクトを新規作成する。

---
+++ UIDescription/ListRender/src/App.jsx
@@ -0,0 +1,20 @@
+import ListMap from "./List";
+import ListFilter from "./ListFilter";
+
+function App() {
+
+  return (
+    <div>
+      <div>
+        <h3>Map</h3>
+        <ListMap />
+      </div>
+      <div>
+        <h3>Filter</h3>
+        <ListFilter />
+      </div>
+    </div>
+  );
+}
+
+export default App
